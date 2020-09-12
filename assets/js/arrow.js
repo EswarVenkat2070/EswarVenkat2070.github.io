452eeff91736c7237130e411e57f9c19320ae321
Eswaran B V <47360371+EswarVenkat2070@users.noreply.github.com>
2020-09-13 04:11:39 +0530

    Add files via upload

--- assets/js/arrow.js
+++ assets/js/arrow.js
@@ -1,14 +1,16 @@
-jQuery(document).ready(function( $ ) {
+(function($) {
+  "use strict";
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
     }
   });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+});
